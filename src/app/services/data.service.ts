d243a46075c53c2699881495a00e24a0cc504b50
Joan Gil <Joan Gil>
2017-03-21 01:19:31 -0400

    added Cross domain request to api

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -9,6 +9,7 @@ import 'rxjs/add/operator/map';
 
 @Injectable()
 export class DataService implements CanActivate{
+  baseUrl = 'https://acseg.herokuapp.com';
 
 
   private headers = new Headers({ 'Content-Type': 'application/json', 'charset': 'UTF-8' });
@@ -17,101 +18,101 @@ export class DataService implements CanActivate{
   constructor(private http: Http, private router: Router) { }
 
    getPeopleLogs(): Observable<any> {
-    return this.http.get('/api/logs/persona').map(res => res.json());
+    return this.http.get('https://acseg.herokuapp.com/api/logs/persona').map(res => res.json());
   }
 
   getMonthlyUsers(): Observable<any> {
-    return this.http.get('/api/logs/month').map(res => res.json());
+    return this.http.get('https://acseg.herokuapp.com/api/logs/month').map(res => res.json());
   }
 
   getHourlyUsers(): Observable<any> {
-    return this.http.get('/api/logs/hour').map(res => res.json());
+    return this.http.get('https://acseg.herokuapp.com/api/logs/hour').map(res => res.json());
   }
    getVehicleLogs(): Observable<any> {
-    return this.http.get('/api/logs/vehiculo').map(res => res.json());
+    return this.http.get('https://acseg.herokuapp.com/api/logs/vehiculo').map(res => res.json());
   }
 
      getAtlantico(): Observable<any> {
-    return this.http.get('/api/statistics/psede/Atlantico').map(res => res.json());
+    return this.http.get('https://acseg.herokuapp.com/api/statistics/psede/Atlantico').map(res => res.json());
   }
     getVillaAsia(): Observable<any> {
-    return this.http.get('/api/statistics/psede/Villa Asia').map(res => res.json());
+    return this.http.get('https://acseg.herokuapp.com/api/statistics/psede/Villa Asia').map(res => res.json());
   }
      getInformatica(): Observable<any> {
-    return this.http.get('/api/statistics/pcarrera/Informatica').map(res => res.json());
+    return this.http.get('https://acseg.herokuapp.com/api/statistics/pcarrera/Informatica').map(res => res.json());
   }
     getContaduria(): Observable<any> {
-    return this.http.get('/api/statistics/pcarrera/Contaduría').map(res => res.json());
+    return this.http.get('https://acseg.herokuapp.com/api/statistics/pcarrera/Contaduría').map(res => res.json());
   }
     getFiscales(): Observable<any> {
-    return this.http.get('/api/statistics/pcarrera/Cs.Fiscales').map(res => res.json());
+    return this.http.get('https://acseg.herokuapp.com/api/statistics/pcarrera/Cs.Fiscales').map(res => res.json());
   }
     getIndustrial(): Observable<any> {
-    return this.http.get('/api/statistics/pcarrera/Industrial').map(res => res.json());
+    return this.http.get('https://acseg.herokuapp.com/api/statistics/pcarrera/Industrial').map(res => res.json());
   }
 
     getPercentIndustrial(): Observable<any> {
-    return this.http.get('/api/statistics/pcarrerap/Industrial').map(res => res.json());
+    return this.http.get('https://acseg.herokuapp.com/api/statistics/pcarrerap/Industrial').map(res => res.json());
   }
 
       getPercentFiscales(): Observable<any> {
-    return this.http.get('/api/statistics/pcarrerap/Cs.Fiscales').map(res => res.json());
+    return this.http.get('https://acseg.herokuapp.com/api/statistics/pcarrerap/Cs.Fiscales').map(res => res.json());
   }
 
       getPercentContaduria(): Observable<any> {
-    return this.http.get('/api/statistics/pcarrerap/Contaduría').map(res => res.json());
+    return this.http.get('https://acseg.herokuapp.com/api/statistics/pcarrerap/Contaduría').map(res => res.json());
   }
 
       getPercentInformatica(): Observable<any> {
-    return this.http.get('/api/statistics/pcarrerap/Informatica').map(res => res.json());
+    return this.http.get('https://acseg.herokuapp.com/api/statistics/pcarrerap/Informatica').map(res => res.json());
   }
 
       getVehiclePercentIndustrial(): Observable<any> {
-    return this.http.get('/api/statistics/vcarrerap/Industrial').map(res => res.json());
+    return this.http.get('https://acseg.herokuapp.com/api/statistics/vcarrerap/Industrial').map(res => res.json());
   }
 
       getVehiclePercentFiscales(): Observable<any> {
-    return this.http.get('/api/statistics/vcarrerap/Cs.Fiscales').map(res => res.json());
+    return this.http.get('https://acseg.herokuapp.com/api/statistics/vcarrerap/Cs.Fiscales').map(res => res.json());
   }
 
       getVehiclePercentContaduria(): Observable<any> {
-    return this.http.get('/api/statistics/vcarrerap/Contaduria').map(res => res.json());
+    return this.http.get('https://acseg.herokuapp.com/api/statistics/vcarrerap/Contaduria').map(res => res.json());
   }
 
       getVehiclePercentInformatica(): Observable<any> {
-    return this.http.get('/api/statistics/vcarrerap/Informatica').map(res => res.json());
+    return this.http.get('https://acseg.herokuapp.com/api/statistics/vcarrerap/Informatica').map(res => res.json());
   }
 
 
      getTotalVehicles(): Observable<any> {
-    return this.http.get('/api/statistics/onCampus/vehicles').map(res => res.json());
+    return this.http.get('https://acseg.herokuapp.com/api/statistics/onCampus/vehicles').map(res => res.json());
   }
 
 
      getTotalPeople(): Observable<any> {
-    return this.http.get('/api/onCampus/count').map(res => res.json());
+    return this.http.get('https://acseg.herokuapp.com/api/onCampus/count').map(res => res.json());
   }
 
     getVehiclesInformatica(){
-      return this.http.get('/api/statistics/vcarrera/Informatica').map(res => res.json());
+      return this.http.get('https://acseg.herokuapp.com/api/statistics/vcarrera/Informatica').map(res => res.json());
 
     }
 
     getVehiclesContaduria(){
-      return this.http.get('/api/statistics/vcarrera/Contaduria').map(res => res.json());
+      return this.http.get('https://acseg.herokuapp.com/api/statistics/vcarrera/Contaduria').map(res => res.json());
     }
 
     getVehiclesFiscales(){
-      return this.http.get('/api/statistics/vcarrera/Cs.Fiscales').map(res => res.json());
+      return this.http.get('https://acseg.herokuapp.com/api/statistics/vcarrera/Cs.Fiscales').map(res => res.json());
     }
 
      getVehiclesIndustrial(){
-      return this.http.get('/api/statistics/vcarrera/Industrial').map(res => res.json());
+      return this.http.get('https://acseg.herokuapp.com/api/statistics/vcarrera/Industrial').map(res => res.json());
     }
 
 
     login(username: string, password: string) {
-        return this.http.post('/api/authuser', { username: username, password: password })
+        return this.http.post('https://acseg.herokuapp.com/api/authuser', { username: username, password: password })
             .map((response: Response) => {
                 let user = response.json();
                 if (user && (user.authsucess="true")) {
